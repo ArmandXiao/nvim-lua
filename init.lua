61590caf5c2502fa20662d1016ed5adc0db47546
ArmandXiao <xiao_lingao@outlook.com>
2021-10-15 23:27:08 +0800

    add compileRun function in lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require('key_binding')
 require('config')
 require('plugin')
+require('function.compile')
 
 --- * LSP * ---
 require('lsp.lsp-setup')
---
+++ lua/function/compile.lua
@@ -0,0 +1,30 @@
+local keymap = vim.api.nvim_set_keymap
+
+local exec = vim.api.nvim_command
+
+function CompileRun()
+    exec("w")
+    if vim.bo.filetype == 'c' then
+        exec("!g++ % -o %<")
+        exec("!time ./%<")
+    elseif vim.bo.filetype == 'cpp' then
+        exec("set splitbelow")
+        exec("!g++ % -Wall -o %<")
+        exec("sp")
+        exec("res -15")
+        exec("term ./%<")
+    elseif vim.bo.filetype == 'java' then
+        exec("set splitbelow")
+        exec("sp")
+        exec("res -5")
+        exec("term javac % && time java %<")
+    elseif vim.bo.filetype == 'sh' then
+        exec("!time bash %")
+    elseif vim.bo.filetype == 'python' then
+        exec("set splitbelow")
+        exec("sp")
+        exec("term python3 %")
+    end
+end
+
+keymap('n', 'R', ":lua CompileRun()<CR>" , {noremap = true})
